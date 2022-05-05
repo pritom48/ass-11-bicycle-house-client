1688f47afc6b58e74e71c29b59be5168a61b7a4a
pritom48 <pritamdas41148@gmail.com>
2022-05-05 11:40:59 +0600

    header added

--- src/Home/Header/Header.js
+++ src/Home/Header/Header.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h2 className='text-info'>this is headers</h2>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-6 d-flex justify-content-center align-items-center flex-column'>
+                    <h1 className='text-info mb-5'>Best Warehouse For <span className='text-dark'><b>Bicycle!</b></span></h1>
+                    <p className='text-center'>We stored best bicycle in the world! Our bicycle is always different from others. we have best unique bicycle the world. We have unique colours unique styles and so on. Find your best bicycle warehouses here!</p>
+                    <button type="button" class="btn btn-info mt-5">Stored</button>
+                </div>
+                <div className='col-md-6'>
+                    <div>
+                        <img className='img-fluid' src="https://html.design/demo/jetcycle/images/slider-img.png" alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
